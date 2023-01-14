e0d8de69663eb99dbe35b4ffa156c428e563d0ee
wustMeiming <wustdb@gmail.com>
2023-01-14 10:15:58 +0800

    add alias

--- env.sh
+++ env.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
 MY_SELF=$(readlink -f $0)
 ROOT_DIR=$(dirname $MY_SELF)
+
+# bin目录加入环境变量
 export PATH=$ROOT_DIR/bin:$PATH
+
+# 加载别名文件
+[ -f $ROOT_DIR/.alias ] && source $ROOT_DIR/.alias
